db55a5b0012e0a0d1204f358335282f9f8b92609
Balaji <vijaybalajiiiii2001@gmail.com>
2023-04-11 14:36:00 +0530

    Modified Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
+import './bootstrap.min.css'
+import { Container } from 'react-bootstrap'
 
 const App = () => {
   return (
     <>
       <Header />
       <main>
-        <h1>Welcome to Proshop</h1>
+        <Container>
+          <h1>Welcome to Proshop</h1>
+        </Container>
       </main>
       <Footer />
     </>
